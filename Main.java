245881a308db464760aa1afe5eb3f8ced7595309
Ayushman Kumar <aayushmanvyahut@gmail.com>
2026-05-27 08:23:36 +0000

    Completed Task 2

--- Main.java
+++ Main.java
@@ -2,17 +2,48 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        
+
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Enter your name: ");
-        String name = sc.nextLine();
+        System.out.print("Enter first number: ");
+        double num1 = sc.nextDouble();
 
-        System.out.print("Enter your age: ");
-        int age = sc.nextInt();
+        System.out.print("Enter second number: ");
+        double num2 = sc.nextDouble();
 
-        System.out.println("Hello " + name + "! Welcome to Java Programming.");
-        System.out.println("You are " + age + " years old.");
+        System.out.print("Enter operator (+, -, *, /): ");
+        char op = sc.next().charAt(0);
+
+        double result;
+
+        switch(op) {
+            case '+':
+                result = num1 + num2;
+                System.out.println("Result: " + result);
+                break;
+
+            case '-':
+                result = num1 - num2;
+                System.out.println("Result: " + result);
+                break;
+
+            case '*':
+                result = num1 * num2;
+                System.out.println("Result: " + result);
+                break;
+
+            case '/':
+                if(num2 != 0) {
+                    result = num1 / num2;
+                    System.out.println("Result: " + result);
+                } else {
+                    System.out.println("Error: Division by zero is not allowed.");
+                }
+                break;
+
+            default:
+                System.out.println("Invalid operator!");
+        }
 
         sc.close();
     }
