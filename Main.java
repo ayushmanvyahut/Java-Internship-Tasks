dabcd79ed86ea6ce948166d8ca7db0a57616f08c
Ayushman Kumar <aayushmanvyahut@gmail.com>
2026-05-27 08:18:43 +0000

    Completed Task 1

---
+++ Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter your name: ");
+        String name = sc.nextLine();
+
+        System.out.print("Enter your age: ");
+        int age = sc.nextInt();
+
+        System.out.println("Hello " + name + "! Welcome to Java Programming.");
+        System.out.println("You are " + age + " years old.");
+
+        sc.close();
+    }
+}
